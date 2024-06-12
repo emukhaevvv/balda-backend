bdc018e41baeb257cf22c08c7b501334d5022659
emukhaevvv <emukhaevvv@yandex.ru>
2024-06-12 13:48:09 +0300

    Add websockets

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { Elysia } from "elysia";
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
+  .ws("/ws", {
+    message(ws, message) {
+      console.log(message);
+      ws.send(message);
+    },
+  })
   .listen(process.env.PORT || 3000);
 
 console.log(
